5a7b6166c2abcd16e4e16549a353b953c75a8db3
gontos <gontosranca@gmail.com>
2024-10-13 21:15:57 +0700

    tambahan-definisi

---
+++ resources/views/components/definisi/hepatozoonosis.blade.php
@@ -0,0 +1,44 @@
+hepatozoonosis adalah penyakit yang ditularkan melalui kutu pada anjing dan kucing yang terabaikan tetapi baru muncul.
+Penyakit ini disebabkan oleh berbagai spesies protozoa yang termasuk dalam genus Hepatozoon dan ditandai dengan gambaran
+klinis yang bervariasi, dari tanda-tanda subklinis dan samar hingga tanda-tanda yang berpotensi mengancam jiwa. Data
+tentang biologi, epidemiologi, ciri-ciri klinis, dan pengobatan hepatozoonosis pada anjing dan kucing masih terbatas dan
+penyakit ini sering kali diabaikan dalam pengaturan klinis. Artikel ini membahas literatur terkini tentang aspek klinis
+hepatozoonosis pada anjing dan kucing, untuk meningkatkan kesadaran akan penyakit ini.
+Penyakit yang ditularkan melalui vektor pada anjing dan kucing yang ditularkan oleh artropoda, misalnya kutu, caplak,
+lalat pasir, dan nyamuk, disebabkan oleh agen infeksius, termasuk virus, bakteri, protozoa, dan cacing. Agen-agen ini
+dapat menyebabkan gambaran klinis subklinis hingga berpotensi fatal dan beberapa berpotensi zoonosis. Meskipun penyakit
+yang ditularkan melalui vektor sangat relevan dalam pengobatan hewan kecil, beberapa di antaranya masih diabaikan dan
+diremehkan. Hal ini terjadi pada hepatozoonosis anjing dan kucing, yang merupakan penyakit yang ditularkan melalui
+vektor yang disebabkan oleh berbagai spesies protozoa dari genus Hepatozoon. Spesies dalam genus ini tidak sepenuhnya
+spesifik spesies, karena Hepatozoon americanum menginfeksi anjing, Hepatozoon felis dan Hepatozoon silvestris
+menginfeksi kucing, sementara Hepatozoon canis (spesies yang paling banyak dikenal) menginfeksi kedua hewan.
+Siklus hidup H. canis tidak langsung, dan anjing (inang perantara) terinfeksi melalui konsumsi kutu yang terinfeksi
+(inang definitif) yang mengandung oosit dewasa. Setelah konsumsi, sporozoit menembus dinding usus dan mencapai berbagai
+organ (termasuk hati, limpa, kelenjar getah bening, otot, dan sumsum tulang) tempat mereka berkembang menjadi meront
+yang melepaskan mikromerozoit Parasit menyerang monosit dan neutrofil dan menjadi gamont, yang diambil oleh kutu saat
+mereka memakan hewan yang terinfeksi Siklus hidup H. americanum mirip dengan H. canis, meskipun spesies ini berkembang
+di otot rangka dan jantung, membentuk kista yang melepaskan merozoit dewasa dan menimbulkan respons peradangan yang
+hebat. Merozoit berkembang menjadi gamon di dalam leukosit dan siklus hidup berlanjut seperti yang dijelaskan di atas
+Siklus biologis H. felis dan H. silvestris belum dijelaskan. Kemungkinan spesies kucing Hepatozoon memiliki biologi yang
+mirip dengan H. canis, meskipun H. felis dapat menyerang jantung dan otot rangka.
+Dampak Terhadap Kucing
+Pada kucing H. felis menimbulkan respons peradangan ringan pada otot miokardium dan otot rangka, dengan tanda-tanda
+subklinis atau non-spesifik, yaitu hipertermia, penyakit kuning, anoreksia, nyeri tekan pada perut, kelesuan,
+limfadenomegali, dan temuan laboratorium, yaitu anemia, limfopenia, neutropenia ringan, trombositopenia, peningkatan
+kadar kreatin kinase, serum laktat dehidrogenase, kreatinin, haptoglobin, serum amiloid A (terutama pada hewan yang
+menunjukkan tanda-tanda klinis) dan penurunan kadar paraoxonase-1. Perubahan klinis yang parah, misalnya anemia
+hemolitik, ikterus, gagal ginjal atau paraparesis, telah dijelaskan pada hewan yang terinfeksi retrovirus atau
+mikoplasma. Variabilitas genetik H. felis mengarah pada hipotesis bahwa genotipe yang berbeda memiliki patogenisitas
+dan/atau biologi yang berbeda. Penelitian lebih lanjut diperlukan untuk mengonfirmasi hipotesis ini.
+Telah dilaporkan kasus miokarditis fatal dengan edema paru yang disebabkan oleh H. silvestris pada kucing domestic.
+Dalam kasus terbaru lainnya, H. silvestris ditemukan di dalam vili usus dari nodul bertangkai endoluminal jejunum yang
+menyebabkan intususepsi usus. Tidak ada data klinis lain yang tersedia mengenai infeksi yang disebabkan oleh H.
+silvestris.
+Diagnosis dan pengobatan
+Hepatozoonosis harus dicurigai pada hewan apa pun dengan tanda-tanda klinis yang sesuai dan perubahan nilai laboratorium
+yang dideteksi menggunakan hitung darah lengkap dan biokimia, akses luar ruangan, dan pengobatan yang tidak teratur atau
+tidak ada terhadap ektoparasit. Diagnosis dapat dikonfirmasi dengan pengamatan langsung gamont Hepatozoon elipsoidal 11
+μm x 5,5 μm dalam sitoplasma neutrofil dan monosit dalam apusan darah yang diwarnai dengan pewarnaan Giemsa,
+May–Grunwald Giemsa atau Romanowsky. Mengingat bahwa pemeriksaan mikroskopis memiliki beberapa keterbatasan karena
+sering kali terdapat tingkat parasitemia yang rendah (sebagian kecil neutrofil yang bersirkulasi menunjukkan gamont),
+parasit dapat dideteksi secara molekuler dan diidentifikasi dari darah tepi.
---
+++ resources/views/components/definisi/koksidiosis.blade.php
@@ -0,0 +1,46 @@
+Apa itu koksidiosis?
+Koksidiosis adalah infeksi saluran usus yang disebabkan oleh organisme bersel tunggal (protozoa) yang disebut coccidia.
+Spesies Isospora adalah jenis coccidia yang paling umum dan terkadang disebut Cystoisospora . Karena coccidia hidup di
+saluran usus dan dapat menyebabkan diare, mereka sering disalahartikan sebagai cacing usus, tetapi coccidia bukanlah
+cacing. Mereka mikroskopis dan bertindak sebagai parasit yang hidup di dalam sel-sel yang melapisi usus.
+Bagaimana kucing saya terinfeksi?
+Kucing Anda kemungkinan besar terinfeksi coccidia karena menelan oosit (koksidia yang belum matang) yang ditemukan dalam
+feses kucing dan lingkungan apa pun yang terkontaminasi feses. Oosit dikeluarkan melalui feses kucing yang terinfeksi.
+Oosit hidup di lingkungan dan akhirnya tumbuh menjadi oosit yang lebih berkembang yang dapat menyebabkan infeksi atau
+infeksi ulang. Pematangan ini dapat terjadi hanya dalam waktu delapan jam tetapi biasanya memakan waktu tujuh hingga
+sepuluh hari. Bentuk oosit yang lebih matang juga menular ke mamalia lain, seperti tikus. Jika kucing Anda menelan
+oosit, oosit tersebut akan matang di usus untuk melengkapi siklus hidupnya. Jika tikus menelan oosit, oosit tersebut
+akan membentuk kista di organ tikus, dan kucing Anda dapat terinfeksi dengan memakan tikus yang terinfeksi.
+Anak kucing biasanya didiagnosis dengan koksidiosis, bukan sejak lahir. Setelah lahir, anak kucing dapat terinfeksi oleh
+induknya melalui paparan kotorannya, terutama karena anak kucing cenderung suka menjelajah. Koksidiosis sangat berbahaya
+bagi anak kucing karena sistem kekebalan tubuhnya belum berkembang. Jika induknya mengeluarkan oosit, bayinya dapat
+menelan oosit tersebut saat menyusui.
+Karena anak kucing (berusia kurang dari enam bulan) tidak memiliki kekebalan terhadap coccidia, organisme tersebut dapat
+berkembang biak dalam jumlah besar, yang dapat menimbulkan konsekuensi serius. Penyakit ini berkembang dalam waktu
+sekitar 3-11 hari setelah anak kucing pertama kali menelan coccidia. Akibatnya, anak kucing yang sakit akibat infeksi
+coccidia berusia setidaknya dua minggu.
+Meskipun sebagian besar kasus disebabkan oleh infeksi dari induknya, hal ini tidak selalu terjadi. Anak kucing yang
+terinfeksi dapat menular ke anak kucing lainnya. Di tempat pengembangbiakan, tempat penampungan hewan, kandang, dan area
+lain tempat banyak hewan peliharaan dapat berdekatan, sebaiknya pisahkan hewan yang terinfeksi dari hewan yang tidak
+terinfeksi.
+Apa tanda-tanda klinis koksidiosis?
+Banyak kucing yang terinfeksi coccidia tidak mengalami diare atau tanda klinis lainnya. Bila oosit ditemukan dalam tinja
+kucing tanpa diare, umumnya dianggap sebagai temuan sementara dan tidak signifikan. Namun, pada anak kucing dan kucing
+dewasa yang lemah, coccidiosis dapat menyebabkan diare encer atau berlendir yang parah, dehidrasi, gangguan perut,
+kehilangan nafsu makan, dan muntah. Dalam kasus yang parah, kematian dapat terjadi.
+Bagaimana koksidiosis didiagnosis?
+Koksidiosis didiagnosis dengan melakukan pemeriksaan mikroskopis pada sampel tinja. Infeksi dengan beberapa parasit
+koksidiosis yang kurang umum dapat didiagnosis dengan tes darah khusus.
+Bagaimana koksidiosis diobati?
+Obat yang paling umum digunakan untuk mengobati koksidiosis adalah antibiotik golongan sulfa, sulfadimethoxine (Albon®).
+Obat ini biasanya diberikan selama 5-20 hari. Beberapa formula obat ini memiliki rasa yang enak dan sebagian besar
+kucing akan menerimanya dengan mudah. ​​Jika obat sulfa tidak efektif, tersedia pengobatan lain, termasuk antibiotik
+sulfa yang dikombinasikan dengan obat lain, amprolium (Albac/Amprol Hi-E®), atau penggunaan ponazuril (Marquis®) atau
+toltrazuril (Baycox®) yang tidak sesuai label. Pengobatan suportif tambahan mungkin diperlukan jika terjadi diare dan
+dehidrasi.
+"Kucing sering terinfeksi ulang dari lingkungan..."
+Kucing sering terinfeksi ulang dari lingkungan, jadi disinfeksi penting dilakukan. Oosit sangat tahan terhadap kondisi
+lingkungan dan disinfektan. Penggunaan pemutih klorin encer, satu cangkir (250 ml) pemutih dicampur dengan satu galon
+(3,8 L) air, efektif. Pastikan untuk menguji pembersihan area kecil dari bahan yang terkena karena pemutih dapat merusak
+banyak permukaan. Pembersihan uap juga dapat digunakan untuk menghancurkan oosit. Pastikan untuk membuang feses secepat
+mungkin dari lingkungan untuk mencegah infeksi ulang.
