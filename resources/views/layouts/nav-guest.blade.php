ce2b803837208cac2241b350ad5888955b3cbda5
gontos <gontosranca@gmail.com>
2024-11-23 08:29:27 +0700

    tambah isi dari pengobatan
    selesain semua frontend

--- resources/views/layouts/nav-guest.blade.php
+++ resources/views/layouts/nav-guest.blade.php
@@ -1,7 +1,9 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 class="text-center">Pendeteksi Penyakit Kucing</h1>
         <div class="flex justify-between h-16">
+
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
--- resources/views/pertolongan.blade.php
+++ resources/views/pertolongan.blade.php
@@ -10,188 +10,72 @@
             <table class="table-fixed w-full">
                 <thead>
                     <tr class="text-left">
-                        <th class="px-4 py-3 text-xs font-medium text-gray-100 uppercase">ID Obat
+                        <th class="px-4 py-3 text-xs font-medium text-gray-100 uppercase">No
                         </th>
-                        <th class="px-4 py-3 text-xs font-medium text-gray-100 uppercase tracking-wider">Nama Obat</th>
                         <th class="px-4 py-3 text-xs font-medium text-gray-100 uppercase tracking-wider">Penyakit Kucing
+                        </th>
+                        <th class="px-4 py-3 text-xs font-medium text-gray-100 uppercase tracking-wider">Obat yang diperlukan
                         </th>
                     </tr>
                 </thead>
 
                 <tbody class="text-left">
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O1</td>
-                        <td class="px-4 py-3 text-sm text-gray-500">Ormetoprim<br>Tribrisen<br>Tortrazuril</td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Koksidiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">1</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Koksidiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Ormetoprim<br>Tribrisen<br>Tortrazuril</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O2</td>
-                        <td class="px-4 py-3 text-sm text-gray-500">Injeksi<br>Diminazene<br>Isometamedi</td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Tripanosomiasis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">2</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Tripanosomiasis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Injeksi<br>Diminazene<br>Isometamedi</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O3</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">3</td>
                         <td class="px-4 py-3 text-sm text-gray-500">
+                            Hepatozoonosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">
                             Trimetho<br>Pyrimethami<br>Clindamycin<br>Decoquinate</td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Hepatozoonosis</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O4</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Babesiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">4</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Babesiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Clindamycin<br></td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O5</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Distemper</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">5</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Distemper</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Cairan<br>Injeksi<br>Vitamin B</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O6</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Ankilostomiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">6</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Ankilostomiosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Telmin/Cane</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O7</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Tungau Otodectes</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">7</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Tungau Otodectes</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Dipping<br>Injeksi</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Phthiriasis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">8</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Phthiriasis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Memandikan<br>Larutan<br>Bayticol</td>
                     </tr>
 
                     <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800">Pedikulosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">9</td>
+                        <td class="px-4 py-3 text-sm text-gray-500">Pedikulosis</td>
+                        <td class="px-4 py-3 text-sm text-gray-800">Selamektin<br>Injeksi</td>
                     </tr>
 
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
 
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
-
-                    <tr>
-                        <td class="px-4 py-3 text-sm text-gray-500">O8</td>
-                        <td class="px-4 py-3 text-sm text-gray-500"></td>
-                        <td class="px-4 py-3 text-sm text-gray-800"></td>
-                    </tr>
         </div>
 </x-guest-layout>
