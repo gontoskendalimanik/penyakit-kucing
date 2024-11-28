bce2e68202472c0d6072cfb883b6ed855c6c4b94
gontos <gontosranca@gmail.com>
2024-11-28 15:45:59 +0700

    tambahan UI
    perbaiki Analisa dan daftar penyakit

--- resources/views/daftar_penyakit.blade.php
+++ resources/views/daftar_penyakit.blade.php
@@ -5,127 +5,125 @@
         </h2>
     </x-slot>
 
-    <div class="flex flex-col w-1/2 py-4">
-        <div class="table-container">
-            <table class="table table-fixed w-full">
-                <thead>
-                    <tr class="text-center text-base text-gray-100 w-3/4">
-                        <th class="px-4 py-3 uppercase" colspan="1">No
-                        </th>
-                        <th class="px-4 py-3 uppercase tracking-wider">
-                            Daftar
-                            Penyakit</th>
-                        <th class="px-4 py-3 uppercase tracking-wider">
-                            Pilih
-                        </th>
-                    </tr>
-                </thead>
+    <div class="flex min-h-screen py-4 items-center justify-center">
+        <table class="table table-auto border-collapse border-slate-800 ">
+            <thead>
+                <tr class="text-center text-base text-gray-800 w-3/4 px-4 py-3">
+                    <th class="px-4 py-3 uppercase border-rounded border border-gray-800">No
+                    </th>
+                    <th class="px-4 py-3 uppercase tracking-wider border border-gray-800">
+                        Daftar
+                        Penyakit</th>
+                    <th class="px-4 py-3 uppercase tracking-wider border border-gray-800">
+                        Pilih
+                    </th>
+                </tr>
+            </thead>
 
-                <tbody class="text-left text-gray-800 text-base">
-                    <tr>
-                        <td class="px-4 py-3">1</td>
-                        <td class="px-4 py-3">Koksidiosis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
-                                    href="pertolongan/koksidiosis">
-                                    Lihat </a>
-                            </button>
-                        </td>
-                    </tr>
+            <tbody class="text-left text-gray-800 text-base px-4 py-3 border border-gray-800">
+                <tr>
+                    <td class="px-4 py-3">1</td>
+                    <td class="px-4 py-3">Koksidiosis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
+                                href="pertolongan/koksidiosis">
+                                Lihat </a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">2</td>
-                        <td class="px-4 py-3">Tripano Somiasis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
-                                    href="#">
-                                    Lihat </a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">2</td>
+                    <td class="px-4 py-3">Tripano Somiasis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
+                                href="#">
+                                Lihat </a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">3</td>
-                        <td class="px-4 py-3">Hepatozoonosis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
-                                    href="#">
-                                    Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">3</td>
+                    <td class="px-4 py-3">Hepatozoonosis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md"><a
+                                href="#">
+                                Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">4</td>
-                        <td class="px-4 py-3">Babesiosis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">4</td>
+                    <td class="px-4 py-3">Babesiosis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">5</td>
-                        <td class="px-4 py-3">Distemper</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">5</td>
+                    <td class="px-4 py-3">Distemper</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">6</td>
-                        <td class="px-4 py-3">Ankilostomiosis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">6</td>
+                    <td class="px-4 py-3">Ankilostomiosis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">7</td>
-                        <td class="px-4 py-3">Tungau Otodectes</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">7</td>
+                    <td class="px-4 py-3">Tungau Otodectes</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">8</td>
-                        <td class="px-4 py-3">Phthiriasis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="px-4 py-3">8</td>
+                    <td class="px-4 py-3">Phthiriasis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td class="px-4 py-3">9</td>
-                        <td class="px-4 py-3">Pedikulosis</td>
-                        <td class="px-4 py-3">
-                            <button type="button"
-                                class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
-                                <a href="#">Lihat</a>
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                <tr>
+                    <td class="px-4 py-3">9</td>
+                    <td class="px-4 py-3">Pedikulosis</td>
+                    <td class="px-4 py-3">
+                        <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 rounded-md">
+                            <a href="#">Lihat</a>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     <x-footer></x-footer>
 </x-guest-layout>
--- resources/views/diagnosa.blade.php
+++ resources/views/diagnosa.blade.php
@@ -5,4 +5,39 @@
         </h2>
     </x-slot>
 
+    <div class="flex items-center justify-center min-h-screen py-4">
+        <div class="bg-white bg-opacity-80 p-6 rounded-lg shadow-lg w-3/4">
+
+            <div class="p-4">
+                <div class="mb-4">
+                    <h2 class="font-semibold">Data Pasien :</h2>
+                    <p>nama : cinta</p>
+                    <p>kelamin : pria</p>
+                    <p>alamat : alamanda</p>
+                    <p>pemilik : yaya</p>
+                </div>
+
+                <div class="mb-4">
+                    <h2 class="font-semibold">hasil analisa terakhir :</h2>
+                    <p>nama latin : tripanosomiasis</p>
+                    <p>gejala :</p>
+                    <ul class="list-decimal list-inside">
+                        <li>malas bergerak</li>
+                        <li>anemik</li>
+                        <li>anoreksia</li>
+                        <li>epifora</li>
+                    </ul>
+
+                </div>
+                <div>
+                    <h2 class="font-semibold">definisi</h2>
+                    <p>merupakan penyakit yang menyerang pada hewan kucing yang disebabkan oleh protozoa trypanozooma
+                        evansi. Adapun gejala-gejalanya kucing akan mengalami malas bergerak karena badan terasa lemas,
+                        anemik, anoreksia, epifora. terapi penyembuhan dan pengobatan dg injeksi suramin, diminazene
+                        aceturat, isometamedium.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-app-layout>
