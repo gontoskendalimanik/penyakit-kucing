d0aee05e4309b9d44e7c6f9cbdaac474095e180d
gontos <gontosranca@gmail.com>
2024-12-10 18:18:01 +0700

    lanjut perbaiki 1
    woke

--- resources/views/daftar_penyakit/pedikulosis.blade.php
+++ resources/views/daftar_penyakit/pedikulosis.blade.php
@@ -1,28 +1,37 @@
 <x-guest-layout>
-<x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Pedikulosis') }}
-    </h2>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Pedikulosis') }}
+        </h2>
 
-    kutu menempel pada bulu dan menyebabkan kerontokan pada rambut karena merusak lapisan epidermis. Ketika dilakukan
-    pemeriksaan mikroskop, ditemukan adanya kutu (lice) jenis Ctenocephalides fellis.
-    Penyakit ini menyerang kitten (anak kucing) maupun kucing dewasa. Umumnya penularan disebabkan karena terjatuhnya
-    kutu (lice) yang kemudian terbawa oleh hewan saat berjalan.
-    Pengobatan yang dapat dilakukan yaitu memberikan Frontline / Selamectin spot on (obat penghilang tungau/kutu),
-    vitamin megaderm (vitamin untuk bulu), dan mandi kutu.
-    Berikut adalah beberapa gejala pedikulosis pada kucing:
-    Gatal-gatal dan kegelisahan
-    Menggaruk, menggosok, dan menggigit area yang terinfeksi
-    Bulu yang kasar, tidak merata, dan kering
-    Rambut yang kusut tebal
-    Papula dan kerak kecil
-    Anemia dan kelemahan pada infestasi yang parah
-    Pedikulosis pada kucing dapat diobati dengan obat anti ektoparasit. Anda dapat menggunakan sampo, kalung, semprotan,
-    atau debu yang membunuh kutu. Jangan gunakan insektisida pada kucing tanpa membicarakannya dengan dokter hewan.
-    Pediculosis adalah penyakit kulit pada kucing yang disebabkan oleh kutu (lice). Pada umumnya.
-    <img src="public/img/pedikulosis.png" alt="Pedikulosis">
+        <p class="indent-8">kutu menempel pada bulu dan menyebabkan kerontokan pada rambut karena merusak lapisan
+            epidermis. Ketika dilakukan
+            pemeriksaan mikroskop, ditemukan adanya kutu (lice) jenis Ctenocephalides fellis.</p><br>
+        <p class="indent-8">Penyakit ini menyerang kitten (anak kucing) maupun kucing dewasa. Umumnya penularan
+            disebabkan karena
+            terjatuhnya
+            kutu (lice) yang kemudian terbawa oleh hewan saat berjalan.</p>
+        <br>
+        <p class="indent-8">Pengobatan yang dapat dilakukan yaitu memberikan Frontline / Selamectin spot on (obat
+            penghilang tungau/kutu),
+            vitamin megaderm (vitamin untuk bulu), dan mandi kutu.</p>
+        <br>
+        Berikut adalah beberapa gejala pedikulosis pada kucing : <br>
+        - Gatal-gatal dan kegelisahan <br>
+        - Menggaruk, menggosok, dan menggigit area yang terinfeksi <br>
+        - Bulu yang kasar, tidak merata, dan kering <br>
+        - Rambut yang kusut tebal <br>
+        - Papula dan kerak kecil <br>
+        - Anemia dan kelemahan pada infestasi yang parah <br> <br>
+        <p class="indent-8">Pedikulosis pada kucing dapat diobati dengan obat anti ektoparasit. Anda dapat menggunakan
+            sampo, kalung,
+            semprotan,
+            atau debu yang membunuh kutu. Jangan gunakan insektisida pada kucing tanpa membicarakannya dengan dokter
+            hewan.
+            Pediculosis adalah penyakit kulit pada kucing yang disebabkan oleh kutu (lice). Pada umumnya.
+            <img src="public/img/pedikulosis.png" alt="Pedikulosis">
 
-    Gambar Pedikulosis
+            Gambar Pedikulosis
 
-</x-slot>
+    </x-slot>
 </x-guest-layout>
